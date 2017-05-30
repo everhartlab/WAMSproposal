67de81ef8a6f16d2e2244ea3d6341c3c7a69f7ec
sporangia <everhart@unl.edu>
2017-05-30 09:37:01 -0500

    Summarizing selected populations

--- PopSummary.R
+++ PopSummary.R
@@ -2,8 +2,26 @@ library(readr)
 library(readxl)
 library(magrittr)
 
-res <- read_excel("", sheet="single", na = c("Populations-in-NE.xlsx", "NA")) %>%
+pops <- read_excel("Populations-in-NE.xlsx", col_names = FALSE, na = c("", "NA")) %>%
   write_csv("NEpops.csv")
 
+colnames(pops) <- c("city", "pop")
+
+
 ##  RFA limits the small towns to being 50,000 or less
+## Proposed start date of September 30th
+
+tot <- sum(pops$pop)  #197 towns total
+sum(pops$pop > 50000) # 3 are >50,000
+sum(pops$pop < 50000) # 194 are <50,000
+
+194/197
+
+Lex <- grep("Lexington", pops$city, fixed = FALSE)
+Nor <- grep("Norfolk", pops$city, fixed = FALSE)
+Win <- grep("Winnebago", pops$city, fixed = FALSE)
+Sco <- grep("Scottsbluff", pops$city, fixed = FALSE)
+Cre <- grep("Crete", pops$city, fixed = FALSE)
+
+pops$percentile <- ecdf(pops$pop)(pops$pop)
 
